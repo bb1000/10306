15cf6f3a7eb4bca38a5ef345dcee05ac486b78cc
Olav Vahtras <vahtras@kth.se>
2026-03-31 11:36:25 +0200

    after L4

---
+++ main.py
@@ -0,0 +1,7 @@
+def main(): # function definition
+    print("Hello from bb1000!")
+
+print("dunder (double underscore) name", __name__)
+
+if __name__ == "__main__":
+    main()  # run the function
